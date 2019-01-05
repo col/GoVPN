597f3774fd76c84ff46186cf02cefff70a5f48ad
Colin Harris <col.w.harris@gmail.com>
2019-01-05 13:39:20 +0800

    Initial Commit

---
+++ GoVPN/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GoVPN
+//
+//  Created by Colin Harris on 5/1/19.
+//  Copyright © 2019 Colin Harris. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GoVPN/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GoVPN
+//
+//  Created by Colin Harris on 5/1/19.
+//  Copyright © 2019 Colin Harris. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
