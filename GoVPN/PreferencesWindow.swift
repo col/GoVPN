e27194c6d84691f2202b22965a6f94a5dff37d37
Colin Harris <col.w.harris@gmail.com>
2019-03-30 17:45:07 +0800

    Fix issue with preferences window not appearing in front of other apps

--- GoVPN/PreferencesWindow.swift
+++ GoVPN/PreferencesWindow.swift
@@ -12,8 +12,8 @@ class PreferencesWindow: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
-    
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        
+        self.window?.makeKeyAndOrderFront(self)
     }
 
 }
